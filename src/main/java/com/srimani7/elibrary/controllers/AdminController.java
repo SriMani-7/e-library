86c82bf6369fa224d6dbc8946c2c71d96a2fedca
Sri Manikanta <srimanikanta1001@gmail.com>
2024-05-31 15:56:35 +0530

    admin dashboard

--- src/main/java/com/srimani7/elibrary/controllers/AdminController.java
+++ src/main/java/com/srimani7/elibrary/controllers/AdminController.java
@@ -1,7 +1,10 @@
 package com.srimani7.elibrary.controllers;
 
+import com.srimani7.elibrary.repositories.BookRepository;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public AdminController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @PostMapping("/login")
     public String login(@RequestParam String password, HttpSession httpSession) {
@@ -20,9 +30,13 @@ public class AdminController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "admin-dashboard";
     }
 
-
+    @GetMapping("/addbook")
+    public String addBookPage() {
+        return "admin-addbook";
+    }
 }
